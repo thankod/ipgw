66c34fa55ebb332a3df0dda19df0bedd92825a0f
thankod <126470629@qq.com>
2017-08-10 11:30:38 +0800

    修改BUG

--- ipgw.py
+++ ipgw.py
@@ -1,11 +1,17 @@
 import requests
 url1 = 'http://ipgw.neu.edu.cn/srun_portal_pc.php?ac_id=1&'
-url2 = 'http://ipgw.neu.edu.cn/include/auth_action.php?k=72013'
-data1 = {"username":"yourid", "password":"yourpw","action":"login", "ac_id": 1, "save_me": 0}
-requests.post(url1, data = data1)
+url2 = 'http://ipgw.neu.edu.cn/include/auth_action.php?k=5874'
+url3 = 'http://ipgw.neu.edu.cn/include/auth_action.php'
+login_data = {"username":"username", "password":"password","action":"login", "ac_id": 1, "save_me": 0}
+logout_data = {"username":"username", "password":"password","action":"logout", "ajax": 1}
+info_data = {"action":"get_online_info", "key": "5874"}
+headers = {"User_Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36"}
 
-data2 = {"action":"get_online_info", "key": "72013"}
-t = requests.post(url2, data = data2)
+requests.post(url3, data = logout_data, headers = headers)
+
+requests.post(url1, data = login_data, headers = headers)
+
+t = requests.post(url2, data = info_data, headers = headers)
 
 
 list = t.text.split(',')
